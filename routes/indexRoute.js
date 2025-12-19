80d346148e0203930a2ba614c9f86a027bd89b0b
kaushal-33 <varmakaushal33@gmail.com>
2025-12-19 17:39:28 +0530

    added update and delete function

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const { home, blogForm, addBlog } = require("../controllers/indexController.js");
+const { home, blogForm, addBlog, deleteBlog, editForm, editBlog } = require("../controllers/indexController.js");
 const { isLogin } = require("../middleware/protectedRoute.js");
 const upload = require("../middleware/multer.js");
 const router = express.Router();
 
 router.get("/", isLogin, home);
-router.get("/add-blog", blogForm);
+router.get("/add-blog", isLogin, blogForm);
 router.post("/add-blog", upload.single("blogImage"), addBlog)
+router.get("/delete-blog/:id", deleteBlog);
+router.get("/edit-form/:id", editForm);
+router.post("/edit-blog/:id", upload.single("blogImage"), editBlog);
 module.exports = router;
